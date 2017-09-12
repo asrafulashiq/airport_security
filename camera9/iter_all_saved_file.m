38af50b2d13184696b124fb06bef7ca22a45bf44
ashraful islam <ashrafulislam@ash-mac.local>
2017-09-12 00:15:39 -0400

    load,update,save

--- camera9/iter_all_saved_file.m
+++ camera9/iter_all_saved_file.m
@@ -5,8 +5,11 @@ for file_number_str = all_file_nums
     file_number = char(file_number_str); % convert to character array
     
     file_to_save = fullfile(file_number, ['camera9_' file_number '_vars.mat']);
-    m = matfile(file_to_save,'Writable',true );
-    m.start_f = 100;
+    
+    eval( ['!cp ' file_to_save ' '  file_to_save '.backup'] );
+    
+    %m = matfile(file_to_save,'Writable',true );
+    %m.start_f = 100;
     
     
 end
