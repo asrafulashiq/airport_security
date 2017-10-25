5743dba395041e4c0aee56d96c0b21ca586b950e
ashraful islam <ashrafulislam@rpi-wl-1573.dynamic.rpi.edu>
2017-10-25 17:11:12 -0400

    ////

--- camera9/mycode_person/a_solve_bin_combine_tracking.m
+++ camera9/mycode_person/a_solve_bin_combine_tracking.m
@@ -47,7 +47,7 @@ for file_number_str = all_file_nums
     %% file to save variables
     file_to_save = fullfile('..',file_number, ['camera9_' file_number '_vars.mat']);
     
-    start_fr = 2610;
+    start_fr = 2500;
     
     if my_decision == is_update_region
         load(file_to_save);
@@ -63,9 +63,6 @@ for file_number_str = all_file_nums
     
     %% region setting,find region position
     
-    load(fullfile('..','Experi1A','r1.mat'));
-    load(fullfile('..','Experi1A','r4.mat'));
-    % load('region_pos2.mat');
     
     % Region1: droping bags
     R_dropping.r1 = [996 1396 512 2073] * scale; %r1;%[103 266 61 436];
@@ -124,17 +121,17 @@ for file_number_str = all_file_nums
         im_c = imresize(img,scale);%original image
         im_c = imrotate(im_c, rot_angle);
         
-        if frame_count >= 2638
+        if frame_count >= 2490
             1;
         end
         
         % tracking the people
-        [people_seq, people_array, R_dropping] = a_peopletracking2(im_c,R_dropping,...
-            R_belt,people_seq,people_array, bin_array);
+        %[people_seq, people_array, R_dropping] = a_peopletracking2(im_c,R_dropping,...
+        %    R_belt,people_seq,people_array, bin_array);
         
         % tracking the bin
-        %[bin_seq, bin_array, R_belt] = a_solve_bin_bin_tracking_2(im_c,R_dropping,...
-        %   R_belt,bin_seq,bin_array, people_array);
+        [bin_seq, bin_array, R_belt] = a_solve_bin_bin_tracking_2(im_c,R_dropping,...
+           R_belt,bin_seq,bin_array, people_array);
         
         title(num2str(frame_count));
         
